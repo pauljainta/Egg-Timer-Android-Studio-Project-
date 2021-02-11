77d7f00aecf800013c21a0562e163eb2178ac35e
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-02-11 23:15:45 +0600

    Completed

--- app/src/main/java/com/example/appeggtimer/MainActivity.java
+++ app/src/main/java/com/example/appeggtimer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appeggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -13,10 +14,55 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
     Button button;
     TextView textView;
+    CountDownTimer countDownTimer;
 
 
-    public void Go(View view)
+    public void GO(View view)
     {
+
+
+
+        if(button.getText().toString().equalsIgnoreCase("GO")) {
+            button.setText("STOP");
+
+             countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    int time_in_seconds = (int) (millisUntilFinished / 1000);
+
+                    String time = (time_in_seconds / 60) + ":" + (time_in_seconds % 60);
+                   // if (time_in_seconds % 60 < 10) time = time + "0";
+                    if (time_in_seconds / 60 < 10) time = "0" + time;
+
+                    textView.setText(time);
+                }
+
+                @Override
+                public void onFinish() {
+
+                   
+
+                }
+            };
+
+          countDownTimer.start();
+
+        }
+        else
+        {
+           if(countDownTimer!=null)  countDownTimer.cancel();
+
+            button.setText("GO");
+            seekBar.setProgress(10);
+            seekBar.setMax(2000);
+            String time=(seekBar.getProgress()/60)+":"+(seekBar.getProgress()%60);
+            //     if(progress%60 <10) time=time+"0";
+            if(seekBar.getProgress()/60 <10) time="0"+time;
+
+            textView.setText(time);
+
+
+        }
 
     }
 
@@ -36,7 +82,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                //textView.setText();
+                String time=(progress/60)+":"+(progress%60);
+           //     if(progress%60 <10) time=time+"0";
+                if(progress/60 <10) time="0"+time;
+
+                textView.setText(time);
             }
 
             @Override
