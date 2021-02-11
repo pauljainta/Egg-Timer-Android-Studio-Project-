08ca1dfcd33dd464818eff85e66e5db964925728
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-02-11 14:20:06 +0600

    No real progress yet

--- app/src/main/java/com/example/appeggtimer/MainActivity.java
+++ app/src/main/java/com/example/appeggtimer/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.appeggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    Button button;
+    TextView textView;
+
+
+    public void Go(View view)
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar=findViewById(R.id.seekBar);
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
+
+        seekBar.setProgress(10);
+        seekBar.setMax(2000);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                //textView.setText();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+
+            }
+        });
+
     }
 }
